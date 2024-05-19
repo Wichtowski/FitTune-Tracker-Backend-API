bcbcdc6898f7c14c34e03c0bc9b638589a01378b
Oskar Wichtowski <oskar.wichtowski3@gmail.com>
2024-05-19 02:57:03 +0200

    refactor: Update routes for adding and deleting exercises from playlists

--- routes/root.js
+++ routes/root.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const app = express();
+const cookieJWT = require('../middlewares/cookieJWT');
 
 router.get('^/$|/index(.html)?', (req, res) => {
-    const filePath = path.join(__dirname, '../../../fitTune/fitTune-Frontend/fitTune-Frontend/src/app/app.component.html');
+    const filePath = path.join(__dirname, '../index.html');
     res.sendFile(filePath);
 });
 
@@ -17,9 +18,9 @@ app.use('/fitTune-api/exercise/create', require('./exercise.routes/create'));
 app.use('/fitTune-api/exercise/edit', require('./exercise.routes/edit')); 
 
 app.use('/fitTune-api/playlist/create', require('./playlist.routes/create'));
-app.use('/fitTune-api/playlist/addExercise', require('./playlist.routes/add'));
+app.use('/fitTune-api/playlist/addExercise', require('./playlist.routes/addExercise'));
 app.use('/fitTune-api/playlist/deleteExercise', require('./playlist.routes/deleteExercise'));
-app.use('/fitTune-api/playlist/updateDescriptionPlaylist', require('./playlist.routes/updateDescription'));
+app.use('/fitTune-api/playlist/updateDescription', require('./playlist.routes/updateDescription'));
 
 module.exports = router;
 module.exports = app;
